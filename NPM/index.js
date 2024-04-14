be4d94856641f719a4a5b607ec58086a2f85eca1
Harsh Vardhan <harshvardhan1412002@gmail.com>
2024-04-15 00:19:28 +0530

    first

---
+++ NPM/index.js
@@ -0,0 +1,7 @@
+//var generateName = require('sillyname');
+//import generateName from "sillyname";
+//import generateName from "superhero";
+import superheroes from 'superheroes';
+//ar sillyName = generateName();
+
+console.log(`im ${superheroes.random()}.`);
